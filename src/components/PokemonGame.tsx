810e719454019e0ec5a13064e4c445713b198072
LuisBrito-11 <luis.brito@eia.edu.co>
2025-04-26 19:27:13 -0500

    Arreglo4ultimo

--- src/components/PokemonGame.tsx
+++ src/components/PokemonGame.tsx
@@ -14,7 +14,7 @@ type PokemonDisplayProps = {
 const PokemonDisplay: React.FC<PokemonDisplayProps> = ({ sprite, gameOver }) => {
   return (
     <div className="mb-6 relative w-40 h-40 mx-auto">
-      <div className="absolute inset-0 rounded-full bg-gradient-to-b from-gray-900 to-black shadow-inner opacity-80"></div>
+      <div className="absolute inset-0 "></div>
       <div
         className={`w-40 h-40 mx-auto transition-all duration-500 ${
           gameOver ? 'filter brightness-100 scale-110' : 'filter brightness-0 scale-100'
